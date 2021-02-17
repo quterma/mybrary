e28dcf2cac2cdf8271203cba5cf2364caa10ba09
Quterma <wolf921@mail.ru>
2021-02-17 13:57:47 +0300

    initial setup

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const { response } = require("express");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+	res.render("index");
+});
+
+module.exports = router;
